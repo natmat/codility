e5a84cb61699e44b9908744c03c59c13751cfa04
Nathan <nathan@mattick.name>
2017-04-28 21:58:06 +0100

    Update the method to not require B[] matrix storage

--- src/codility/CyclicRotation.java
+++ src/codility/CyclicRotation.java
@@ -3,8 +3,8 @@ package codility;
 public class CyclicRotation {
 	public static void main(String[] args) {
 		CyclicRotation cr = new CyclicRotation();
-		final int[] A = {}; //3,8,9,7,6};
-		final int K = 0;
+		final int[] A = {1,2,3,4,5};
+		final int K = 3;
 		int[] B = cr.solution(A, K);
 		for (int i: B) {
 			System.out.print(i + ",");
@@ -12,15 +12,23 @@ public class CyclicRotation {
 	}
 
 	public int[] solution(int[] A, int K) {
+		System.out.println("Solution()");
 		if ((0 == K) || (0 == A.length)) {
 			return(A);
 		}
-		int shift = A.length - K % A.length;
-		int[] B = new int[A.length];
-		for (int i = 0 ; i < A.length ; i++) {
-			B[i] = A[(i+shift)%A.length];
+
+		for (int i = 0 ; i < K ; i++) {
+			for (int j = 0 ; j < A.length ; j++) {
+				swap(A, 0, j);
+			}
 		}
-		return(B);
+		return(A);
+	}
+	
+	private static void swap(int[] A, int i, int j) {
+		int tmp = A[i];
+		A[i] = A[j];
+		A[j] = tmp;
 	}
 }
 
