2271f62e7fac53b3997f4c116fe88fbbe23a53dc
Nathan <nathan@mattick.name>
2017-05-04 06:47:12 +0100

    Score of 100%, now need to find out why previous commit failed.

--- src/codility/TapeEquilibrium.java
+++ src/codility/TapeEquilibrium.java
@@ -8,22 +8,22 @@ public class TapeEquilibrium {
 	}
 
 	public int solution(int[] A) {
-		long sum = 0;
+		int sum = 0;
 		for (int i : A) {
 			sum += i;
 		}
-		int min = Integer.MAX_VALUE;
-		long sumL = 0;
-		long sumR = sum;
-		for (int i : A) {
-			sumL += i;
-			sumR -= i;
-			if (0 == sumR) { break ; }
-			int diff = (int)Math.abs(sumL - sumR);
+		int sumL = A[0];
+		int sumR = sum - sumL;
+		int min = Math.abs(sumL - sumR);
+
+		for (int p = 1 ; p < A.length-1 ; p++) {
+			sumL += A[p];
+			sumR -= A[p];
+			int diff = Math.abs(sumL - sumR);
 			if (min > diff) {
 				min = diff;
 			}
-//			System.out.println("L="+sumL+",sumR="+sumR+",diff="+diff+",min="+min);
+			//		 			System.out.println("L="+sumL+",sumR="+sumR+",diff="+diff+",min="+min);
 		}
 		return(min);
 	}
