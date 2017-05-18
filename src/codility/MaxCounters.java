b59a09ccee61155614b28bce3bf924238a3f2fe1
Nathan <nathan@mattick.name>
2017-05-18 07:11:07 +0100

    100% minimised the number of max updates

--- src/codility/MaxCounters.java
+++ src/codility/MaxCounters.java
@@ -9,21 +9,19 @@ public class MaxCounters {
 
 	public int[] solution(int N, int[] A) {
 		int max = 0;
-		int sumMax = 0;
 		int[] B = new int[N];
 		for (int i : A) {
 			if (i <= N) {
+				B[i-1] = Math.max(max,  B[i-1]);
 				B[i-1]++;
 				max = Math.max(max, B[i-1]);
 			}
 			else {
 				B = new int[N];
-				sumMax += max;
-				max = 0;
 			}
 		}
 		for (int i = 0 ; i < N ; i++) {
-			B[i] += sumMax;
+			B[i-1] = Math.max(max,  B[i-1]);
 		}
 
 		// show(B);
