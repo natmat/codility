9e9aa30449d4d67c11caea5f1d21350e62086466
Nathan <nathan@mattick.name>
2017-06-06 06:48:06 +0100

    Correct counting when A>=i<=B

--- src/codility/CountDiv.java
+++ src/codility/CountDiv.java
@@ -3,30 +3,30 @@ package codility;
 public class CountDiv {
 
 	public static void main(String[] args) {
-		System.out.println(CountDiv.solution(6, 11, 2));
-		System.out.println(CountDiv.solution(10,10, 5));
-		System.out.println(CountDiv.solution(10,10, 7));
-		System.out.println(CountDiv.solution(10,10, 20));
+		System.out.println(CountDiv.solution( 6, 11,  2));
+		System.out.println(CountDiv.solution(10, 10,  5));
+		System.out.println(CountDiv.solution(10, 10,  7));
+		System.out.println(CountDiv.solution(10, 10, 10));
+		System.out.println(CountDiv.solution(10, 10, 20));
+		System.out.println(CountDiv.solution( 5, 10,  4));
+		System.out.println(CountDiv.solution( 5, 10,  5));
+		System.out.println(CountDiv.solution( 5, 10,  6));
 	}
 
 	public static int solution(int A, int B, int K) {
 		int n = 0;
+		
 		if (A == 0) n++;
 		if (K>B) return(n);
 		
 		int first;
-		if (A == K) {
-			first = A;
-		}
-		else {
-			first = (int)(A/K)*K;
-			if (first < A) first += K;
-		} 
+		first = (int)(A/K)*K;
+		if (first < A) first += K;
 
-		if ((first >= A) && (first <= B)) n++;
+		if (first == A) n++;
 
 		int diff = B-first;
-		n += (diff/K);
+		n += (int)(diff/K);
 
 		return(n);
 	}
