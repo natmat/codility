f4650d363c69f08053928a908c5180e85f7a6472
Nathan <nathan@mattick.name>
2017-06-06 06:56:22 +0100

    50% spotted that wasn't counting first divisor.

--- src/codility/CountDiv.java
+++ src/codility/CountDiv.java
@@ -3,6 +3,8 @@ package codility;
 public class CountDiv {
 
 	public static void main(String[] args) {
+		System.out.println(CountDiv.solution(11,345,17));
+
 		System.out.println(CountDiv.solution( 6, 11,  2));
 		System.out.println(CountDiv.solution(10, 10,  5));
 		System.out.println(CountDiv.solution(10, 10,  7));
@@ -14,7 +16,7 @@ public class CountDiv {
 	}
 
 	public static int solution(int A, int B, int K) {
-		int n = 0;
+ 		int n = 0;
 		
 		if (A == 0) n++;
 		if (K>B) return(n);
@@ -23,7 +25,7 @@ public class CountDiv {
 		first = (int)(A/K)*K;
 		if (first < A) first += K;
 
-		if (first == A) n++;
+		if ((first >= A) && (first <= B)) n++;
 
 		int diff = B-first;
 		n += (int)(diff/K);
