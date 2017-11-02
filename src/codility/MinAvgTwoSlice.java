777e140e2b01ed51957919f5ee7f6277fda2592f
Nathan <nathan@mattick.name>
2017-11-02 06:28:07 +0000

    Still looking at terminating all threads.

--- src/codility/MinAvgTwoSlice.java
+++ src/codility/MinAvgTwoSlice.java
@@ -9,14 +9,12 @@ public class MinAvgTwoSlice {
 	}
 
 	public int solution(int[] A) {
-		double a = 10000;
+		double a = (A[0] + A[1])/2;
 		int i = 0;
-		for (int j = 0 ; j < A.length-1 ; j++) {
+		for (int j = 2 ; j < A.length ; j++) {
 			double tmp = a;
-			a = Math.min(a, (A[j] + A[j+1])/2.0);
-			if (j < A.length-2) {
-				a = Math.min(a, (A[j] + A[j+1] + A[j+2])/3.0);
-			}
+			a = Math.min(a, (A[j-2] + A[j-1] + A[j])/3.0);
+			a = Math.min(a, (A[j-1] + A[j])/2.0);
 			if (a < tmp) {
 				i = j;
 			}
