5a79e96c113299b490e0bb3c48cf0dd500e3b54e
Nathan <nathan@mattick.name>
2017-11-22 07:30:35 +0000

    2nd attempt.

--- src/codility/GeometricRangeQuery.java
+++ src/codility/GeometricRangeQuery.java
@@ -1,43 +1,47 @@
 package codility;
 
-import java.util.Arrays;
+// you can also use imports, for example:
+import java.util.*;
+import java.lang.String.*;
+import java.math.*;
 
-public class GeometricRangeQuery {
-	public static void main(String[] args) {
-		String S = "CAGCCTA";
-		int[] P = {2,5,0};
-		int[] Q = {4,5,6};
-		
-		int[] answer = solution(S, P, Q);
-		for (int i : answer) { System.out.println(i + " "); } System.out.println();
-	}
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
 
-	public static int[] solution(String S, int[] P, int[] Q) {
-		int[] out = new int[P.length];
+class Solution {
+	public int[] solution(String S, int[] P, int[] Q) {
+		int M = P.length;
+		int[] out = new int[M];
 		Arrays.fill(out, 5);
-		int[] end = new int[P.length];
-		Arrays.fill(end, 1);
 
-		for (int i = 0 ; i < S.length() ; i++) {
-			char c = S.charAt(i);
-			for (int n = 0 ; n < P.length ; n++) {
-				if ((out[n] != 1) && (P[n] <= i) && (i <= Q[n])) {
-					out[n] = Math.min(out[n], dnaToInt(c));
+		int i, j, c;
+		for (i = 0 ; i < S.length() ; i++) {
+			c = getValue(S.charAt(i));
+			for (j = 0 ; j < M ; j++) {
+				if (out[j] == 1) {
+					continue;
 				}
-			} 
-			if (Arrays.equals(out, end)) break;
-		}
+
+				if ((i >= P[j]) && (i <= Q[j])) {
+					if (c < out[j]) {
+						out[j] = c;
+					}
+					if (out[j] == 1) {
+						break;
+					}
+				}
+			}            
+		}        
 		return(out);
 	}
 
-	static int dnaToInt(char c) {
-		int out = 5;
+	int getValue(char c) {
 		switch(c) {
-		case 'A': out = 1; break;
-		case 'C': out = 2; break;
-		case 'G': out = 3; break;
-		case 'T': out = 4; break;
+		case 'A': return 1;
+		case 'C': return 2;
+		case 'G': return 3;
+		case 'T': return 4;
 		}
-		return(out);
+		return(5);
 	}
 }
